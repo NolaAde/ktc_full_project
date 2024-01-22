38d6ff1ff2dd791f9856d87447d911b4db41775b
NolaAde <147328045+NolaAde@users.noreply.github.com>
2024-01-22 17:20:15 +0000

    back on the bussiness

---
+++ assignment3.py
@@ -0,0 +1,14 @@
+    print(abs(- 10))
+    print (abs(-10))
+    steps = -3
+    if abs(steps) > 0:
+        print('Character is moving')
+    steps = -3
+    if steps < 0 or steps > 0:
+        print('Character is moving')
+    print(bool(0))
+    print(bool(1))
+    print(bool(1123.23))
+    print(bool(-500))
+    print(bool(None))
+    print(bool('a'))
--- main.py
+++ main.py
@@ -8,17 +8,32 @@ init(autoreset = True)
 
 player_score = 0
 
-def introduction():
-    # Welcome the player
-    print(Fore.LIGHTMAGENTA_EX+"Welcome to python adventure")
-    time.sleep(2)
-    player_name = input(Fore.RED+"What's your name?")
-    time.sleep(2)
-    print(f"hi {player_name}, nice to meet you!")
-    time.sleep(2)
+# def introduction():
+#     # Welcome the player
+#     print(Fore.LIGHTMAGENTA_EX+"Welcome to python adventure")
+#     time.sleep(2)
+#     player_name = input(Fore.RED+"What's your name?")
+#     time.sleep(2)
+#     print(f"hi {player_name}, nice to meet you!")
+#     time.sleep(2)
 
-show = introduction()
+# show = introduction()
 
 def make_choice(question, option, score_change):
     print(question)
+    for i, option in enumerate(option, 1):
+        print(f"{i}. {option}")
+        while True:
+            try:
+                choice = int(input("Enter the number of your choice: "))
+                if 1 <= choice <= len(option):
+                    # update global score variable
+                    global player_score += score_change[choice - 1]
+                return choice
+            else:
+                print("Invalid choice try again")
+        except ValueError:
+               print("invalid input. Enter a number") 
+
+            
 
