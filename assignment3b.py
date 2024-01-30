2500819fb7ff7d5833d58d48f77953d87a631e31
NolaAde <147328045+NolaAde@users.noreply.github.com>
2024-01-30 17:53:24 +0000

    dkdd

---
+++ assignment3b.py
@@ -0,0 +1,12 @@
+my_small_program = '''print('ham')
+print('sandwich')'''
+exec(my_small_program)
+float('12')
+
+your_age = input('Enter your age: ')
+enter_your_age: 20
+age = float(your_age)
+if age > 13:
+    print('You are %s years too old' % (age - 13))
+
+    int(123.456)
--- main.py
+++ main.py
@@ -37,26 +37,33 @@ def make_choice(question, options, score_change):
 
 def forest_path():
     print(Fore.LIGHTMAGENTA_EX+"You come across a fork in the path")
-    time.sleep(1)
+    time.sleep(5)
     choice = make_choice("Which route will you choose", ["Go left", "Go right"], [1, -1])
 
     if choice == 1:
         print(Fore.LIGHTCYAN_EX + "You encounter a friendly cat and she leads the way to the next challenge")
     else:
         print(Fore.LIGHTBLUE_EX + "Oh no, you encountered a venomous spider and you are trapped in his web")
-time.sleep(1)
+time.sleep(5)
 def mountain_climb():
     print("You continue to were you are headed and reach a steep mountain.")
-    
-    time.sleep(1)
+    time.sleep(5)
     choice = make_choice( "what do you do?", ["Use a rope", "Climb without equipment."], [1, -1])
 
     if choice == 1:
         print(Fore.CYAN + "Smart. Using a rope makes it easier to climb")
     else:
         print(Fore.BLUE + "No, you were supposed to use the rope! You slipped but Horimi saved you, lucky you.")
-
-
+time.sleep(2)
+def mystical_cave():
+    print(Fore.LIGHTMAGENTA_EX + "After that adventure, i think you might enjoy resting in a magical cave.")
+    time.sleep(5)
+    choice = make_choice(Fore.LIGHTGREEN_EX + "what do you do?", ["Explore the cave", "Just rest and then leave without exploring"], [1, -1])
+    time.sleep(2)
+    if choice == 1:
+        print(Fore.CYAN + "Wow, you got the power of moving things with your mind")
+    else:
+        print(Fore.BLUE + "Horimi said you could have gotten powers. You turn back to face the cave but it disapeared, poor you.")
 # Main game loop
 def play_game():
     introduction()
@@ -64,6 +71,7 @@ def play_game():
     # Start of the adventure
     forest_path()
     mountain_climb()
+    mystical_cave()
     
 if __name__ == "__main__":
         play_game()
