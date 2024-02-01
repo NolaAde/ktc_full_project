12141efb6b26f3ebf631d5b6fb9c9e4f374403ab
NolaAde <147328045+NolaAde@users.noreply.github.com>
2024-02-01 17:42:46 +0000

    happy

--- assignment3b.py
+++ assignment3b.py
@@ -1,7 +1,10 @@
 my_small_program = '''print('ham')
 print('sandwich')'''
 exec(my_small_program)
+
 float('12')
+
+float('123.456789')
 
 your_age = input('Enter your age: ')
 enter_your_age: 20
@@ -9,4 +12,39 @@ age = float(your_age)
 if age > 13:
     print('You are %s years too old' % (age - 13))
 
-    int(123.456)
+    #int(123.456)
+
+    #int('123')
+
+    #int('123.456')
+
+    #len('this is a test string')
+
+    creature_list = ['unicorn', 'cyclops', 'fairy', 'elf', 'dragon','troll']
+    print(len(creature_list))
+
+    enemies_map = {'Batman' : 'Joker','Superman' : 'Lex Luthor','Spiderman' : 'Green Goblin'}
+    print(len(enemies_map))
+
+    fruit = ['apple', 'banana', 'clementine', 'dragon fruit']
+    length = len(fruit)
+    for x in range(0, length):
+        print('the fruit at index %s is %s' % (x, fruit[x]))
+#     number = [5, 4, 10, 30, 22]
+# print(max(number))
+
+strings = 's,t,r,i,n,g,S,T,R,I,N,G'
+print(max(strings))
+
+
+print(max(10, 300, 450, 50, 90))
+
+numbers = [5, 4, 10, 30, 22]
+print(min(numbers))
+
+guess_this_number = 61
+player_guess = [12, 15, 70, 45]
+if max(player_guess) > guess_this_number:
+    print('Boom! You all lose')
+else:
+    print("You win. Noooooooooooooooooooooooooooooooo")
